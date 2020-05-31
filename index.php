e4471973303df12afc876528d267505e18f18c07
Ashish <spiraldeveloper@gmail.com>
2020-06-01 02:18:43 +0530

    init

--- index.php
+++ index.php
@@ -3,8 +3,6 @@
 #PATH
 define('PATH', exec('echo $HOME/stuffsdk-src/'));
 define('MANIFEST','stuffsdk/manifest.php');
-
 require_once(sprintf('%s%s',PATH,MANIFEST));
-
 #bind
-manifest::bind();
+bind_service();
